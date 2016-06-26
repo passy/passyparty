42511f7b9d452434fea38aa83f83930a7df998b5
Pascal Hartig <phartig@rdrei.net>
2016-06-26 20:53:12 +0100

    Show some stuff

--- src/Main.elm
+++ src/Main.elm
@@ -1,63 +1,65 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Time as Time
 
 
 -- APP
+
+
 main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program { init = init
+                 , view = view
+                 , update = update
+                 , subscriptions = subscriptions }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    { currentTime : Time.Time }
+
+
+init : (Model, Cmd Msg)
+init = ({ currentTime = 0 }, Cmd.none)
 
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+
+type Msg = Tick Time.Time
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        Tick time ->
+            ({ currentTime = time }, Cmd.none)
+
+
+currentTime : Sub Msg
+currentTime = Time.every Time.second Tick
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = currentTime
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
+        [ div [ class "row" ]
+            [ div [ class "col-xs-12" ]
+                [ p [] [ text ("passy-party") ]
+                , pre [] [ text <| toString <| model.currentTime ]
+                ]
+            ]
         ]
-      ]
-    ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// pull in desired CSS/SASS files
-require( './styles/main.scss' );
+'use strict';
 
-// inject bundled Elm app into div#main
-var Elm = require( './Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+require('./styles/main.scss');
+var Elm = require('./Main');
+Elm.Main.embed(document.getElementById('main'));
